53b9a913bee2b215b9fcf449006a3360c04db9ab
Spuggle <immortalwarrior101@gmail.com>
2020-10-23 21:51:12 +0500

    Refactoring
    - Refactor code to functions
    - Removed server as it was unnecessary
    - Renamed output files and their confirmation messages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import http2 from "http2";
 import { PDFDocument, PDFPage } from "pdf-lib";
 import fs from "fs";
 import { promisify } from "util";
 
 
-type CategorizedPages = [ PDFPage[], PDFPage[] ];
-
-const HOME = "E:\\Projects\\pdf-merge";
+const HOME = __dirname.split(/\\|\//g).slice(0, -1).join("\\");
 const file = (ioDir: string, fileName: string) => `${HOME}\\${ioDir}\\${fileName}`;
 const readFilePromise = promisify(fs.readFile);
 const writeFilePromise = promisify(fs.writeFile);
@@ -14,48 +11,94 @@ const writeFilePromise = promisify(fs.writeFile);
 const readInput = (fileName: string) => readFilePromise(file("in", fileName));
 const writeOutput = (fileName: string, data: Uint8Array) => writeFilePromise(file("out", fileName), data);
 
-const server = http2.createServer();
-
 fs.readdir(`${HOME}\\in`, async function(err, pdfFiles) {
   if (err) return console.error(err);
 
-  const loadedPDFBuffers = await Promise.all(pdfFiles.map(pdfFile => readInput(pdfFile)));
-  const pdfDocs = await Promise.all(loadedPDFBuffers.map(loadedPDFBuffer => PDFDocument.load(loadedPDFBuffer)));
-  const PDFs = await Promise.all([
-    PDFDocument.create(),
-    PDFDocument.create()
-  ]);
-  const [ oddPDF, evenPDF ] = PDFs;
+  const loadedPDFBuffers = await loadPDFBuffers(pdfFiles);
+  const inputPDFs = await createPDFsFromBuffers(loadedPDFBuffers);
+  const outputPDFs = await createBlankPDFs();
+  const [ oddPDF, evenPDF ] = outputPDFs;
 
-  const [ oddPages, evenPages ] = (await Promise.all(pdfDocs.map(async pdfDoc => {
-    let pageCount = pdfDoc.getPageCount();
-    if (pageCount % 2 === 1 && ++pageCount) pdfDoc.addPage();
-
-    const sortedPages: [ number[], number[] ] = [ [], [] ];
-
-    for (let i = 0; i < pageCount; ++i) sortedPages[i % 2].push(i);
-
-    return Promise.all(sortedPages.map((sortedPageNumbers, i) => PDFs[i % 2].copyPages(pdfDoc, sortedPageNumbers)));
-  })))
-    .reduce(([ allOdd, allEven ], [ odd, even ]) => {
-      allOdd.push(...odd);
-      allEven.push(...even);
-      
-      return [ allOdd, allEven ];
-    }, [ [], [] ]);
+  const [ oddPages, evenPages ] = await sortAndArrangePDFs(inputPDFs, outputPDFs);
 
     for (const oddPage of oddPages) oddPDF.addPage(oddPage);
     for (const evenPage of evenPages) evenPDF.addPage(evenPage);
 
-    const [ oddBytes, evenBytes ] = await Promise.all([
-      oddPDF.save(),
-      evenPDF.save()
-    ]);
-
-    await Promise.all([
-      writeOutput("even.pdf", evenBytes).then(() => console.log("Outputted even.pdf")),
-      writeOutput("odd.pdf", oddBytes).then(() => console.log("Outputted odd.pdf"))
-    ]);
+    await createPDFs(oddPDF, evenPDF);
 });
 
-server.listen(37475);
+function loadPDFBuffers(pdfFiles: string[]) {
+  return Promise.all(pdfFiles.map(pdfFile => readInput(pdfFile)));
+}
+
+function createPDFsFromBuffers(loadedPDFBuffers: Buffer[]) {
+  return Promise.all(
+    loadedPDFBuffers.map(loadedPDFBuffer => PDFDocument.load(loadedPDFBuffer, {
+      ignoreEncryption: true
+    }))
+  );
+}
+
+async function createBlankPDFs() {
+  return await Promise.all([
+    PDFDocument.create(),
+    PDFDocument.create()
+  ]);
+}
+
+async function sortAndArrangePDFs(inputPDFs: PDFDocument[], outputPDFs: [PDFDocument, PDFDocument]): Promise<[PDFPage[], PDFPage[]]> {
+  return (await Promise.all(inputPDFs.map(sortPages(outputPDFs))))
+    .reduce<[ PDFPage[], PDFPage[] ]>(arrangePages(), [[], []]);
+}
+
+function sortPages(outputPDFs: [PDFDocument, PDFDocument]): (value: PDFDocument, index: number, array: PDFDocument[]) => Promise<PDFPage[][]> {
+  return async (inputPDF) => {
+    let pageCount = inputPDF.getPageCount();
+    if (pageCount % 2 === 1 && ++pageCount)
+      inputPDF.addPage();
+
+    const sortedPages: [number[], number[]] = [[], []];
+
+    //sortedPages[0].push(0);
+
+    for (let i = 0; i < pageCount; ++i)
+      sortedPages[i % 2].push(i);
+
+    return Promise.all(sortedPages.map(copySortedPages(outputPDFs, inputPDF)));
+  };
+}
+
+function arrangePages(): (previousValue: [PDFPage[], PDFPage[]], currentValue: PDFPage[][], currentIndex: number, array: PDFPage[][][]) => [PDFPage[], PDFPage[]] {
+  return ([allOdd, allEven], [odd, even]) => {
+    allOdd.push(...odd);
+    allEven.push(...even);
+
+    return [allOdd, allEven];
+  };
+}
+
+function copySortedPages(PDFs: [PDFDocument, PDFDocument], pdfDoc: PDFDocument): (value: number[], index: number, array: number[][]) => Promise<PDFPage[]> {
+  return (sortedPageNumbers, i) => PDFs[i % 2].copyPages(pdfDoc, sortedPageNumbers);
+}
+
+async function createPDFs(oddPDF: PDFDocument, evenPDF: PDFDocument) {
+  const [oddBytes, evenBytes] = await getPDFBytes(oddPDF, evenPDF);
+
+  await createPDFFromBytes(evenBytes, oddBytes);
+}
+
+async function getPDFBytes(oddPDF: PDFDocument, evenPDF: PDFDocument): Promise<[Uint8Array, Uint8Array]> {
+  return await Promise.all([
+    oddPDF.save(),
+    evenPDF.save()
+  ]);
+}
+
+async function createPDFFromBytes(evenBytes: Uint8Array, oddBytes: Uint8Array) {
+  await Promise.all([
+    writeOutput("Even_Pages.pdf", evenBytes).then(() => console.log("Exported even pages")),
+    writeOutput("Odd_Pages.pdf", oddBytes).then(() => console.log("Exported odd pages"))
+  ]);
+}
+
+type CategorizedPages = [ PDFPage[], PDFPage[] ];
